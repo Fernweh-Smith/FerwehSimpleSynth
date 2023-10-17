db8352be9882b937f62fa57f9770ccde93e41cf0
Fernweh-Smith <104039346+Fernweh-Smith@users.noreply.github.com>
2023-10-17 19:21:12 +1100

    Created factories for gain and adsr parmeter attributes, and adsr normalisable ranges.

--- src/PluginParameters.cpp
+++ src/PluginParameters.cpp
@@ -4,32 +4,65 @@
 
 #include "PluginParameters.h"
 
+
+
 template <typename Param>
-Param& addToGroup( juce::AudioProcessorParameterGroup& group, std::unique_ptr<Param> param){
+static Param& addToGroup( juce::AudioProcessorParameterGroup& group, std::unique_ptr<Param> param){
     auto& paramRef = *param;
     group.addChild(std::move(param));
     return paramRef;
 }
 
-juce::AudioProcessorParameterGroup& addToLayout( juce::AudioProcessorValueTreeState::ParameterLayout& layout, std::unique_ptr<juce::AudioProcessorParameterGroup> param){
+static juce::AudioProcessorParameterGroup& addToLayout( juce::AudioProcessorValueTreeState::ParameterLayout& layout, std::unique_ptr<juce::AudioProcessorParameterGroup> param){
     auto& paramRef = *param;
     layout.add(std::move(param));
     return paramRef;
 }
+
+static juce::AudioParameterFloatAttributes generateGainAttrs(){
+    return juce::AudioParameterFloatAttributes()
+    .withStringFromValueFunction([](float x, int len){
+        return (std::stringstream() << std::fixed << std::setprecision(2) << x << " dB").str();
+    })
+    .withValueFromStringFunction([](const juce::String& str){
+        return str.getFloatValue();
+    });
+}
+
+static juce::NormalisableRange<float> generateNormRangeADSR(){
+    return {0.0f, 4.0f, 0.0, 0.33f};
+}
+
+static juce::AudioParameterFloatAttributes generateTimeAttrs(){
+    return juce::AudioParameterFloatAttributes()
+            .withStringFromValueFunction([](float x, int len){
+                if(x < 1.0f){
+                    return (std::stringstream() << std::fixed << std::setprecision(0) << (x * 1000.0f) << " ms").str();
+                } else {
+                    return (std::stringstream() << std::fixed << std::setprecision(2) << x << " s").str();
+                }
+
+            })
+            .withValueFromStringFunction([](const juce::String& str){
+                return str.getFloatValue();
+            });
+}
+
 
 ParameterReferences::MainGroup::MainGroup(
         paramGroup &group)
         : outGain(addToGroup(group, std::make_unique<floatParam>(IDs::out_gain,
                                                                  "Gain",
                                                                  juce::NormalisableRange<float>(-12.0f, 12.0f),
-                                                                 0.0f)))
+                                                                 0.0f,
+                                                                 generateGainAttrs())))
 {}
 
 ParameterReferences::GeneratorGroup::GeneratorGroup(
         paramGroup &group)
         : wave_type(addToGroup(group, std::make_unique<choiceParam>(IDs::wave_type,
                                                                     "Wave Type",
-                                                                    juce::StringArray(IDs::sin_wave, IDs::sawtooth_wave, IDs::triangle_wave, IDs::square_wave),
+                                                                    juce::StringArray("Sin", "Sawtooth", "Triangle", "Square"),
                                                                     0))),
           shaper_type(addToGroup(group, std::make_unique<choiceParam>(IDs::shaper_type,
                                                                       "Shaper Type",
@@ -46,23 +79,24 @@ ParameterReferences::ADSRGroup::ADSRGroup(
         paramGroup &group)
         : attack(addToGroup(group, std::make_unique<floatParam>(IDs::attack,
                                                                 "Attack",
-                                                                juce::NormalisableRange<float>(0.0f, 4.0f, 0.0, 0.33f),
-                                                                0.1f))),
+                                                                generateNormRangeADSR(),
+                                                                0.01f,
+                                                                generateTimeAttrs()))),
           decay(addToGroup(group, std::make_unique<floatParam>(IDs::decay,
                                                                "Decay",
-                                                               0.0f,
-                                                               1.0f,
-                                                               0.5f))),
+                                                               generateNormRangeADSR(),
+                                                               0.3f,
+                                                               generateTimeAttrs()))),
           sustain(addToGroup(group, std::make_unique<floatParam>(IDs::sustain,
                                                                  "Sustain",
                                                                  0.0f,
                                                                  1.0f,
-                                                                 0.5f))),
+                                                                 0.8f))),
           release(addToGroup(group, std::make_unique<floatParam>(IDs::release,
                                                                  "Release",
-                                                                 0.0f,
-                                                                 1.0f,
-                                                                 0.5f)))
+                                                                 generateNormRangeADSR(),
+                                                                 0.6f,
+                                                                 generateTimeAttrs())))
 {}
 
 ParameterReferences::ParameterReferences(
